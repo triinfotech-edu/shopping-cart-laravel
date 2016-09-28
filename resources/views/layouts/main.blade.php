7e20dd9a09fcba49a1aedc68480cba9baccd5c27
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-09-28 21:07:37 +0530

    First day

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>@yield('title')</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Fontawesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+	<!-- Custom css -->
+	<link rel="stylesheet" href="{{ URL::to('assets/css/app.css') }}">
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><i class="fa fa-shopping-cart"></i> Shopping Cart</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user"></i> User Account <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">SignUp</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>	
+
+<div class="container">
+	@yield('content')
+</div>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,139 @@
+@extends('layouts.main')
+
+@section('title')
+	My Home Page
+@endsection
+
+
+@section('content')
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://media.northgame.co.uk/core/images/products/large/2013_Kookaburra_2014_Kahuna_400_Cricket_Bat.jpg" alt="...">
+      <div class="caption clearfix">
+        <h3>Bat 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>
+        	<div class="pull-left">
+        		<i class="fa fa-rupee fa-2x"></i>
+        		<span class="rupee">700</span>
+        	</div>
+        	<a href="#" class="btn btn-success pull-right" role="button">Button</a>
+    	</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://media.northgame.co.uk/core/images/products/large/2013_Kookaburra_2014_Kahuna_400_Cricket_Bat.jpg" alt="...">
+      <div class="caption clearfix">
+        <h3>Bat 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>
+        	<div class="pull-left">
+        		<i class="fa fa-rupee fa-2x"></i>
+        		<span class="rupee">700</span>
+        	</div>
+        	<a href="#" class="btn btn-success pull-right" role="button">Button</a>
+    	</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://media.northgame.co.uk/core/images/products/large/2013_Kookaburra_2014_Kahuna_400_Cricket_Bat.jpg" alt="...">
+      <div class="caption clearfix">
+        <h3>Bat 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>
+        	<div class="pull-left">
+        		<i class="fa fa-rupee fa-2x"></i>
+        		<span class="rupee">700</span>
+        	</div>
+        	<a href="#" class="btn btn-success pull-right" role="button">Button</a>
+    	</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://media.northgame.co.uk/core/images/products/large/2013_Kookaburra_2014_Kahuna_400_Cricket_Bat.jpg" alt="...">
+      <div class="caption clearfix">
+        <h3>Bat 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>
+        	<div class="pull-left">
+        		<i class="fa fa-rupee fa-2x"></i>
+        		<span class="rupee">700</span>
+        	</div>
+        	<a href="#" class="btn btn-success pull-right" role="button">Button</a>
+    	</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://media.northgame.co.uk/core/images/products/large/2013_Kookaburra_2014_Kahuna_400_Cricket_Bat.jpg" alt="...">
+      <div class="caption clearfix">
+        <h3>Bat 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>
+        	<div class="pull-left">
+        		<i class="fa fa-rupee fa-2x"></i>
+        		<span class="rupee">700</span>
+        	</div>
+        	<a href="#" class="btn btn-success pull-right" role="button">Button</a>
+    	</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img class="img-responsive" src="http://media.northgame.co.uk/core/images/products/large/2013_Kookaburra_2014_Kahuna_400_Cricket_Bat.jpg" alt="...">
+      <div class="caption clearfix">
+        <h3>Bat 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>
+        	<div class="pull-left">
+        		<i class="fa fa-rupee fa-2x"></i>
+        		<span class="rupee">700</span>
+        	</div>
+        	<a href="#" class="btn btn-success pull-right" role="button">Button</a>
+    	</p>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
